c1b32aa12d53ffc51a635f01af4e8af6d5250450
ortal <ortalprpr@gmail.com>
2021-01-05 20:33:48 +0200

    Surprisingly popular

--- ProjectFramework/AggregationMethods/SurprisinglyPopular.py
+++ ProjectFramework/AggregationMethods/SurprisinglyPopular.py
@@ -1,10 +1,31 @@
-import pandas as pd
+import traceback
 from GetProcessedData import get_answer_names
 
 
-def suprisingly_pop_answer(df):
-    answer_names = get_answer_names(df)
-    # Get prediction for each answer
+def surprisingly_pop_answer(df):
+    try:
+        # df = df_tuple[1]
+        # file_name = df_tuple[0]
+        answer_names = get_answer_names(df)
+        max_difference = 0
+        the_answer = "empty"
+        # correct_answer = df[df['Class'] == 1]['Answer'].iloc[0]
+        for answer in answer_names:
+            mean = df[answer].mean()
+            actual = len(df[df['Answer'] == answer])/len(df)
+            if mean < actual:
+                diff = actual - mean
+                if diff > max_difference:
+                    max_difference = diff
+                    the_answer = answer
+        return the_answer
+        # Get prediction for each answer
+        # answer_to_prediction_avg[answer] = df[answer].mean(), len(df[df['Answer'] == str(answer)])
+        # Get the votes for each answer
+        # answer_to_sum[answer] = len(df[df['Answer'] == answer])
+    except Exception as e:
+        print(f"Exception in {file_name}")
+        print(e)
+        traceback.print_tb(e.__traceback__)
 
-    # Get the votes for each answer
-    # Find the suprinsingly popular answer
+
